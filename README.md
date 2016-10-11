2df4be95572faa1e1fc14ad80c4b8951ddd16a7c
akalend <akalend@mail.ru>
2016-10-11 04:03:30 +0300

    add lua script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# subscribe-websockets-nginx-lua
+# Basic
 The subscribe lua script for redis by websocket through nginx
+
+
+# Requitments
+
+ * lua-nginx-module      https://github.com/openresty/lua-nginx-module/  
+ * lua-cjson             https://github.com/openresty/lua-cjson/         
+ * lua-resty-websocket   https://github.com/openresty/lua-resty-websocket
+ * lua-resty-redis       https://github.com/openresty/lua-resty-redis
+
+
+# Install
+
+You must to copy lib folder from:
+
+ *	lua-resty-websocket
+ *	lua-resty-redis
+
+to /usr/share/nginx/lua or othe path
+
+to include in the http location of nginx.conf:
+```
+http {
+ lua_package_path "/usr/share/nginx/lua/lib/?.lua;;";   
+}
+```
+
+Create the location for websocket and include file websocket_server.lua:
+
+```
+ location /ws { 
+            content_by_lua_file /path/to/file/websocket_server.lua; 
+        }
+```
---
+++ websocket-server.lua
@@ -0,0 +1,94 @@
+    local server = require "resty.websocket.server"
+    local redis = require "resty.redis"
+    local cjson = require "cjson"
+    
+    local red = redis:new()
+
+    local ok, err = red:connect("127.0.0.1", 6379)
+    if not ok then
+        ngx.log(ngx.ERR,"1: failed to connect: ", err)
+        return
+    end
+
+    ngx.log( ngx.INFO, "redis: connected")
+
+
+    local wb, err = server:new{
+        timeout = 5000,  -- in milliseconds
+        max_payload_len = 65535,
+    }
+
+    if not wb then
+        ngx.log(ngx.ERR, "failed to new websocket: ", err)
+        return ngx.exit(444)
+    end
+
+    local data, typ, err = wb:recv_frame()
+
+    if not data then
+        ngx.log(ngx.ERR, "failed to receive a frame: ", err)
+        return ngx.exit(444)
+    end
+
+    if typ == "close" then
+
+        local bytes, err = wb:send_close(1000, "enough, enough!")
+        if not bytes then
+            ngx.log(ngx.ERR, "failed to send the close frame: ", err)
+            return
+        end
+        local code = err
+        ngx.log(ngx.INFO, "closing with status code ", code, " and message ", data)
+        return
+    end
+
+    if typ == "text" then
+
+        local res, err = red:subscribe(data)
+        if not res then
+            ngx.log(ngx.ERR,"1: failed to subscribe: ", err)
+            
+            return
+        end
+
+        ngx.log(ngx.INFO,"subscribe: " , cjson.encode(res))
+
+        if not res then
+            ngx.log(ngx.ERR,"1: failed to subscribe: ", err)
+            
+            local bytes, err = wb:send_close(1000, "enough, enough!")
+            if not bytes then
+                ngx.log(ngx.ERR, "failed to send the close frame: ", err)
+                return
+            end
+
+            return
+        end
+
+        res, err = red:read_reply()
+        if not res then
+            ngx.log(ngx.ERR,"failed to read reply from redis: ", err)
+            return
+        end
+
+        ngx.log(ngx.INFO,"subscribe: " , cjson.encode(res))
+
+
+        wb:set_timeout(1000)  -- change the network timeout to 1 second
+        bytes, err = wb:send_text(  res[3] )
+        if not bytes then
+            ngx.log(ngx.ERR, "failed to send a text frame: ", err)
+            return ngx.exit(444)
+        end
+    else
+        ngx.log(ngx.INFO, "received a frame of type ", typ, " and payload ", data)
+    end
+
+    wb:set_timeout(100)  -- change the network timeout to 0.1 second
+
+
+    local bytes, err = wb:send_close(100, "enough, enough!")
+    if not bytes then
+        ngx.log(ngx.ERR, "failed to send the close frame: ", err)
+        return
+    end
